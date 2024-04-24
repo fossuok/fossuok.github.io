553d5fb74ac700e0dd0680c7d9c7030b49d3c713
_MadushaSandaruwan <madushakv@live.com>
2024-04-24 20:38:04 +0530

    Update SpeakerList.js with Weeraman linkedin link

--- src/data/SpeakerList.js
+++ src/data/SpeakerList.js
@@ -21,7 +21,7 @@ const speakerList = [
         title: "Co-Founder & CTO",
         company: "Verdentra",
         image: "/images/summit/speakers/anuradha.jpg",
-        linkedin: "hhttps://www.linkedin.com/in/anuradhaweeraman/",
+        linkedin: "https://www.linkedin.com/in/anuradhaweeraman/",
     },
     {
         slug: "sashika",
